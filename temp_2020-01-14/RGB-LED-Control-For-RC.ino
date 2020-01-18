9e1260c0601524a88e9371047a1b8d97f3fa50f3
EAndocs <47283536+EAndocs@users.noreply.github.com>
2020-01-18 10:26:16 -0500

    Add files via upload

--- temp_2020-01-14/RGB-LED-Control-For-RC.ino
+++ temp_2020-01-14/RGB-LED-Control-For-RC.ino
@@ -2,7 +2,7 @@
 
 #define RC_INPUT_PIN 2
 
-#define TOP_LED_OUTPUT_PIN 12
+#define TOP_LED_OUTPUT_PIN 3
 #define TOP_LED_COUNT 22
 
 #define BOTTOM_LED_OUTPUT_PIN 4
@@ -57,16 +57,106 @@ void setup() {
     }
   }
 
-    // If we made it this far we have a valid PWM signal - flash a green LED to indicate that we're ready to go
-    topLEDs[0] = CRGB(0, 255, 0);
-    bottomLEDs[0] = CRGB(0, 255, 0);
-    FastLED.show();
-    delay(200);
-    topLEDs[0] = CRGB(0, 0, 0);
-    bottomLEDs[0] = CRGB(0, 0, 0);
-    FastLED.show();
+    // If we made it this far we have a valid PWM signal - flash a green LED 3x to indicate that we're ready to go
+    for (int i=0; i<3; i++) {
+      topLEDs[0] = CRGB(0, 255, 0);
+      bottomLEDs[0] = CRGB(0, 255, 0);
+      FastLED.show();
+      delay(200);
+      topLEDs[0] = CRGB(0, 0, 0);
+      bottomLEDs[0] = CRGB(0, 0, 0);
+      FastLED.show();
+      delay(200);
+  }
 }
 
 void loop() {
+  pwmValue = pulseIn(RC_INPUT_PIN, HIGH);
   Serial.println(pwmValue);
- }
+
+  patternNumber = ((pwmValue - 1000) * 10 / 1000) + 1;  // Choose pattern 1 through 10 based on PWM value
+  if (patternNumber > 10) {  // Handle PWM values > 2000
+    patternNumber = 10;
+  }
+  if (patternNumber < 1) {  // This shouldn't ever happen, even with PWM values < 1000
+    patternNumber = 1;
+  }
+  
+  // Pattern 1 - All LEDs off
+  if (patternNumber == 1) {
+    fill_solid(topLEDs, TOP_LED_COUNT, CRGB(0, 0, 0));
+    fill_solid(bottomLEDs, BOTTOM_LED_COUNT, CRGB(0, 0, 0));
+    FastLED.show();
+  }
+
+  // Pattern 2 - LOS Mode - Top LEDs blue and bottom LEDs green
+  if (patternNumber == 2) {
+    fill_solid(topLEDs, TOP_LED_COUNT, CRGB(0, 0, 255));
+    fill_solid(bottomLEDs, BOTTOM_LED_COUNT, CRGB(0, 255, 0));
+    FastLED.show();
+  }
+
+  // Pattern 3 - Cycle all LEDs through all HSV colors
+  if (patternNumber == 3) {
+    for (int i = 0; i <= 255; i++) {
+      for (int j = 0; j < TOP_LED_COUNT; j++) {
+        topLEDs[j] = CHSV(i, 255, 255);
+      }
+      for (int j = 0; j < BOTTOM_LED_COUNT; j++) {
+        bottomLEDs[j] = CHSV(i, 255, 255);
+      }
+      FastLED.show();
+      delay(5);
+    }
+  }
+
+  // Pattern 4 - Static rainbow
+  if (patternNumber == 4) {
+    for (int i = 0; i < TOP_LED_COUNT; i++) {
+      topLEDs[i] = CHSV((192 / (TOP_LED_COUNT - 1)) * i, 255, 255);
+    }
+    for (int i = 0; i < BOTTOM_LED_COUNT; i++) {
+      bottomLEDs[i] = CHSV((192 / (BOTTOM_LED_COUNT - 1)) * i, 255, 255);
+    }
+    FastLED.show();
+  }
+
+  // Pattern 5 - Scrolling rainbow - NEEDS WORK
+
+  if (patternNumber == 5) {
+    for (int i = 0; i < TOP_LED_COUNT; i++) {
+      for (int j = 0; j < TOP_LED_COUNT; j++) {
+        int h = (i + j) % TOP_LED_COUNT;
+        topLEDs[h] = CHSV((192 / (TOP_LED_COUNT - 1)) * j, 255, 255);
+      }
+      FastLED.show();
+      delay(40);
+    }
+      
+    for (int i = 0; i < BOTTOM_LED_COUNT; i++) {
+      for (int j = 0; j < BOTTOM_LED_COUNT; j++) {
+        int h = (i + j) % BOTTOM_LED_COUNT;
+        bottomLEDs[h] = CHSV((192 / (BOTTOM_LED_COUNT - 1)) * j, 255, 255);
+      }
+      FastLED.show();
+     delay(40);
+    }
+  }
+  
+
+
+  // Pattern 6 -  
+
+  // Pattern 7 -
+
+  // Pattern 8 -
+
+  // Pattern 9 -
+  
+  // Pattern 10 - All LED on white
+  if (patternNumber == 10) {
+    fill_solid(topLEDs, TOP_LED_COUNT, CRGB(255, 255, 255));
+    fill_solid(bottomLEDs, BOTTOM_LED_COUNT, CRGB(255, 255, 255));
+    FastLED.show();
+  }
+}
